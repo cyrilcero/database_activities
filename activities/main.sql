95bba728c8ac35811d092b03ceed711f7312e90c
cyrilcero <cyril.cero@gmail.com>
2024-01-29 20:52:39 +0800

    add comments to the file

--- activities/main.sql
+++ activities/main.sql
@@ -1,3 +1,6 @@
+-- DATABASE ACTIVITIES 2024/01/29
+
+-- CREATE TABLE
 CREATE TABLE students (
 	id 				integer 						PRIMARY KEY,
 	first_name 		character varying(100) 			NOT NULL,
@@ -7,6 +10,7 @@ CREATE TABLE students (
 	location 		character varying(100) 			NOT NULL
 );
 
+-- INSERT DATA TO TABLE
 INSERT INTO students (id, first_name, last_name, age, location) VALUES
 (1, 'Juan', 'Cruz', 18, 'Manila'),
 (2, 'Anne', 'Wall', 20, 'Manila'),
@@ -15,17 +19,25 @@ INSERT INTO students (id, first_name, last_name, age, location) VALUES
 (5, 'Zack', 'Matthews', 22, 'Marikina'),
 (6, 'Finn', 'Lam', 25, 'Manila');
 
+-- UPDATE THE TABLE VALUES
 UPDATE students
 SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan'
 WHERE id = 1;
 
+-- DELETE DATA ENTRY
 DELETE FROM students
 WHERE id = 6; -- OR (SELECT MAX(id) FROM students);
 
+-- FOLLOW UP ACTIVITY
+
+-- DISPLAY COUNT OF ALL STUDENTS
 SELECT COUNT(id) AS total_students FROM students;
 
+-- SELECT ALL STUDENTS WITH LOCATION = MANILA
 SELECT * FROM STUDENTS WHERE location = 'Manila';
 
+-- DISPLAY THE AVERAGE AGE OF ALL STUDENTS
 SELECT AVG(age) AS student_average_age FROM STUDENTS;
 
+-- DISPLAY ALL STUDENTS BY AGE DESCENDING ORDER
 SELECT * FROM students ORDER BY age DESC;
