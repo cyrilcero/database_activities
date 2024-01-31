e02e662507a566d30873dce29df798ac6b129534
cyrilcero <cyril.cero@gmail.com>
2024-01-31 18:44:47 +0800

    add solution to classroom database join activity

---
+++ activities/classroom.sql
@@ -0,0 +1,19 @@
+CREATE TABLE classrooms (
+    id INT NOT NULL,
+    student_id INT NOT NULL,
+    section varchar(1) NOT NULL,
+    PRIMARY KEY (id)
+    -- FOREIGN KEY (student_id) REFERENCES students(id)
+);
+INSERT INTO classrooms (id, student_id, section)
+VALUES (1, 1, 'A'),
+    (2, 2, 'A'),
+    (3, 3, 'B'),
+    (4, 4, 'C'),
+    (5, 5, 'B'),
+    (6, 6, 'A'),
+    (7, 7, 'C'),
+    (8, 8, 'B'),
+    (9, 9, 'B'),
+    (10, 10, 'C');
+
